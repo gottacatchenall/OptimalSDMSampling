1d0d58710a6fbf3d7965ad292e5bc84ecef18815
Michael D. Catchen <mdcatchen@gmail.com>
2025-10-14 17:18:40 -0400

    foo

---
+++ alpha_run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --account=def-tpoisot
+#SBATCH --job-name=RandomRangeShiftSampling 
+#SBATCH --output=%x-%A-%a.out
+#SBATCH --nodes=1               
+#SBATCH --ntasks=1               
+#SBATCH --cpus-per-task=1        
+#SBATCH --mem-per-cpu=16G 
+#SBATCH --array=1-512 
+#SBATCH --time=2:00:00         
+
+export JULIA_DEPOT_PATH="/project/def-tpoisot/mcatchen/JuliaEnvironments/OptimalSDMSampling"
+
+module load julia/1.11.3
+julia test_alpha.jl
---
+++ test_alpha.jl
@@ -0,0 +1,80 @@
+using SpeciesDistributionToolkit
+# using CairoMakie
+# using GeoMakie
+using MultivariateStats
+using BiodiversityObservationNetworks
+using Statistics
+using Distributions
+using Random
+using XGBoost
+using LinearAlgebra
+using DataFrames, CSV
+using Logging
+# using ProgressMeter
+
+const BONs = BiodiversityObservationNetworks
+const MV = MultivariateStats
+const SDT = SpeciesDistributionToolkit
+
+
+include(joinpath("src", "bioclim.jl"))
+include(joinpath("src", "niche.jl"))
+include(joinpath("src", "rangeshift.jl"))
+include(joinpath("src", "occurrence.jl"))
+include(joinpath("src", "fnl.jl"))
+#include(joinpath("src", "plotting.jl"))
+include(joinpath("src", "sdm.jl"))
+include(joinpath("src", "metrics.jl"))
+include(joinpath("src", "treatments.jl"))
+
+function main()
+    global_logger(ConsoleLogger(Error));
+
+    method = adaptive_bon
+   
+
+    bioclim = read_bioclim(joinpath("data"))
+
+    Ntotals = 50:50:500
+    alphas = 1:0.5:10
+
+    dfs = []
+
+    #prog = Progress(length(alphas)*length(prop_baseline))
+
+    num_layers = length(bioclim[begin])
+    niche = LogisticNiche(
+        centers = rand(Uniform(0.1, 0.5), num_layers),
+        shapes = rand(Normal(0., 0.3), num_layers)
+    )
+    sr = get_shifting_range(niche, bioclim)
+
+    for tilting in alphas
+        for n in Ntotals
+            df = run_treatment(
+                sr;
+                total_samples = n,
+                tilting = tilting,
+                method = method
+            )
+            df.n_total = fill(n, nrow(df))
+            df.alpha = fill(tilting, nrow(df))
+            push!(dfs, df)
+            #next!(prog)
+        end 
+    end 
+    total_df = vcat(dfs...)
+
+
+    outdir = joinpath("artifacts", "vary_alpha")
+    mkpath(outdir)
+
+    job_id = ENV["SLURM_ARRAY_TASK_ID"]
+    CSV.write(joinpath(outdir, "replicate_$job_id.csv"), total_df)
+
+    return total_df
+end 
+
+
+
+main()
