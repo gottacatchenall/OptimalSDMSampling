ecd9787fc3e030bfa05e36dbe570e740415ee725
Michael D. Catchen <mdcatchen@gmail.com>
2025-10-14 10:10:20 -0400

    ok

--- run.sh
+++ run.sh
@@ -7,9 +7,9 @@
 #SBATCH --cpus-per-task=1        
 #SBATCH --mem-per-cpu=16G 
 #SBATCH --array=1-2 
-#SBATCH --time=30:00         
+#SBATCH --time=10:00         
 
 export JULIA_DEPOT_PATH="/project/def-tpoisot/mcatchen/JuliaEnvironments/OptimalSDMSampling"
 
 module load julia/1.11.3
-julia make_sdms.jl
+julia main.jl
